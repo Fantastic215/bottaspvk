45713ad945d7e751dc9b211552518bd42e0c7c47
Fantastic215 <60544071+Fantastic215@users.noreply.github.com>
2020-05-03 14:26:40 +0300

    Update VK_bot_TEST.py

--- VK_bot_TEST.py
+++ VK_bot_TEST.py
@@ -112,7 +112,7 @@ while True:
                             send(str(exe), peer_id)
 
 
-                elif s == '/info' or s=='Расписание /info':
+                elif s == '/info' or s=='@club194277538 (Расписание) /info':
                     send('бота написал\n'
                          'https://vk.com/fantasticfeed\n'
                          '____________________________\n'
@@ -124,7 +124,7 @@ while True:
                     send('разработка...', peer_id)
 
                 
-                elif s == '/rasp' or s=='Расписание /rasp':
+                elif s == '/rasp' or s=='@club194277538 (Расписание) /rasp':
                     try:
                         
                         a = vk_session.method("photos.getMessagesUploadServer")
